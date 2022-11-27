8e81973c0bdbcd17267d1004193fbe5236cb639f
almer <2171364@gmail.com>
2022-11-27 21:29:41 +0700

    replace react-dnd with react-beautiful-dnd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ModalProvider } from "components/Modal/Global";
 import { StaticPageProps } from "Constants/Pages";
-import dynamic from 'next/dynamic'
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import dynamic from "next/dynamic";
 
 // Dynamically import Auth provider since localStorage object only available during client side render
-const AuthProvider = dynamic(
-  () => import('components/Auth/provider'),
-  { ssr: false }
-)
+const AuthProvider = dynamic(() => import("components/Auth/provider"), {
+  ssr: false,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider pageProps={pageProps}>
       <ModalProvider>
-        <DndProvider backend={HTML5Backend}>
-          <Component {...pageProps} />
-        </DndProvider>
+        <Component {...pageProps} />
       </ModalProvider>
     </AuthProvider>
   );
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,28 @@
 import { useAuth } from "components/Auth/provider";
 import Button from "components/Button";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { StaticPageProps } from "Constants/Pages";
+import { useState } from "react";
+
+const data = {
+  tasks: {
+    "task-1": { id: "task-1", content: "" },
+    "task-2": { id: "task-2", content: "" },
+    "task-3": { id: "task-3", content: "" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To-Do",
+      taskIds: ["task-1", "task-2", "task-3"],
+    },
+  },
+  columnOrder: ["column-1"],
+};
 
 function Dashboard() {
   const auth = useAuth();
+  const [items, setItems] = useState(data);
 
   function logout() {
     auth?.setAuthState("");
@@ -14,6 +33,45 @@ function Dashboard() {
       <Button onPress={logout} type="link">
         <p>Logout</p>
       </Button>
+      <DragDropContext
+        onDragEnd={(a) => {
+          console.log(a.type);
+        }}
+      >
+        <Droppable droppableId="test-1" type="drop">
+          {(provided, snapshot) => (
+            <div>
+              {items.columnOrder.map((column) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  <Draggable draggableId="test-1" index={0} key="aa-1">
+                    {(a) => (
+                      <div
+                        {...a.draggableProps}
+                        {...a.dragHandleProps}
+                        ref={a.innerRef}
+                      >
+                        aaa1
+                      </div>
+                    )}
+                  </Draggable>
+                  <Draggable draggableId="test-2" index={1} key="aa-2">
+                    {(a) => (
+                      <div
+                        {...a.draggableProps}
+                        {...a.dragHandleProps}
+                        ref={a.innerRef}
+                      >
+                        aaa2
+                      </div>
+                    )}
+                  </Draggable>
+                  {provided.placeholder}
+                </div>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
